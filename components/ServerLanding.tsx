bf820d12937becf703d161499f6248dc18d2d9b7
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-12-15 00:54:22 +0530

    feat: create component for /channels/@me

---
+++ components/ServerLanding.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Link from 'next/link';
+import WindowContainer from './WindowContainer';
+import styles from './css/ServerLanding.module.css';
+
+interface ServerLandingProps {
+  username: string,
+  servers: {
+    [serverId: string]: {
+      name: string;
+      channels: object;
+    }
+  }
+}
+
+const ServerLanding: React.FC<ServerLandingProps> = ({ username, servers }) => {
+  return (
+    <div className={styles.container}>
+      <WindowContainer title={`Welcome, ${username || 'User'}!`}>
+        <div className={styles.serverList}>
+          {Object.entries(servers).map(([serverId, server]) => (
+            <Link href={`/channels/${serverId}`}
+              key={serverId}
+              className={styles.serverItem}
+            >
+              <div className={styles.serverIcon}>
+                {server.name[0].toUpperCase()}
+              </div>
+              <div className={styles.serverInfo}>
+                <h3>{server.name}</h3>
+                <p>{Object.keys(server.channels).length} channels</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </WindowContainer>
+    </div>
+  );
+};
+
+export default ServerLanding;
