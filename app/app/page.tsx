256ba3180fe25270c9d420e23c8cfcfd1e65d9a1
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-12-14 18:45:12 +0530

    fix: bump routing logic to route level

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+"use client"
+import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 import LoadingScreen from '@/components/LoadingScreen'
 
 const AppLoading = () => {
-  return <LoadingScreen />
+  const [username, setUsername] = useState("")
+  const router = useRouter()
+
+  useEffect(() => {
+    const loggedUser = localStorage.getItem("username")
+    if (!loggedUser) {
+      router.push("/")
+      return
+    }
+
+    setUsername(loggedUser)
+
+    const timer = setTimeout(() => {
+      router.push("/channels/@me")
+    }, 5000) // Fake loading
+
+    return () => clearTimeout(timer)
+  }, [router])
+
+  return username && <LoadingScreen title={`Welcome, ${username}!`} />
 }
 
 export default AppLoading
--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -1,13 +1,9 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
+import React from "react";
 import WindowContainer from "@/components/WindowContainer";
 import styles from "@/components/css/LoadingScreen.module.css";
 import Marquee from "react-fast-marquee";
 
-const LoadingScreen = () => {
-  const [username, setUsername] = useState("");
-  const router = useRouter();
+const LoadingScreen: React.FC<{ title: string }> = ({ title }) => {
   const tips = [
     "Discord was almost called Bonfire before we picked our name. It was meant to be nice and cozy.",
     "Discord was almost called Wyvern before we picked our name. Not too proud of that one.",
@@ -31,29 +27,14 @@ const LoadingScreen = () => {
     "Our old Partner mascot was an elf named Springle. He recently retired.",
     "You can join up to 100 servers.",
   ];
+  
   const randomTip = tips[Math.floor(Math.random() * tips.length)];
-
-  useEffect(() => {
-    const loggedUser = localStorage.getItem("username");
-    if (!loggedUser) {
-      router.push("/");
-      return;
-    }
-
-    setUsername(loggedUser)
-
-    const timer = setTimeout(() => {
-      router.push("/channels/@me");
-    }, 5000); // Fake loading
-
-    return () => clearTimeout(timer);
-  }, [router]);
 
   return (
     <div className={styles.container}>
       <WindowContainer title="Loading...">
         <div className={styles.content}>
-          <h2 className={styles.greeting}>Welcome{username && ` , ${username}`}!</h2>
+          <h2 className={styles.greeting}>{title}</h2>
           <div className={styles.loader}>
             <Marquee
               direction="right"
