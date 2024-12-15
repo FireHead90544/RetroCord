a318c818b50ca8a308d587063de28a54627acfef
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-12-15 20:15:45 +0530

    feat: update theme

--- components/HeadingLogo.tsx
+++ components/HeadingLogo.tsx
@@ -8,13 +8,13 @@ const HeadingLogo: React.FC<{ onChannelsPage?: boolean }> = ({
     style={{
       fontFamily: "'Comic Sans MS', cursive",
       fontSize: "4rem",
-      color: "#800080",
-      textShadow: "2px 2px 0 #FFFFFF, 4px 4px 0 #000000",
+      color: "#6c81ca",
+      textShadow: "2px 2px 0 #36393e, 4px 4px 0 #000000",
       textAlign: "center",
       marginBottom: onChannelsPage ? "auto" : "20px",
     }}
   >
-    <Link href="/" style={{ color: "#800080", textDecoration: "none" }}>
+    <Link href="/" style={{ color: "#6c81ca", textDecoration: "none" }}>
       Discord
     </Link>
   </div>
